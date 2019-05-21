6d71e2040879ee45b457347a5c29b35d7bcce13c
JesusReynoso12 <jealreynosome@ittepic.edu.mx>
2019-05-20 20:41:33 -0600

    first commit

---
+++ app/src/main/java/reynosojesus/ittepic/edu/tpdm_u4_practica2_jesusreynoso/MainActivity.java
@@ -0,0 +1,13 @@
+package reynosojesus.ittepic.edu.tpdm_u4_practica2_jesusreynoso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new Lienzo(this));
+    }
+}
